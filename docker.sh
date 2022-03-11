94779133bc51da603ec6ededb214d81b38f634de
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-03-11 23:50:02 +0000

    updated docker script

--- docker.sh
+++ docker.sh
@@ -1,3 +1,5 @@
 #!env sh
-# docker run -it -e PYTHONDONTWRITEBYTECODE=1 -w /test -v $PWD:/test python:3.8 bash
-docker run -it -w /test -v "$PWD":/test python:3.8 bash
+docker run -it -e PYTHONDONTWRITEBYTECODE=1 --entrypoint=bash -w /test -v "$PWD":/test python:3.8
+
+# Afterwards:
+# - pip install numpy
