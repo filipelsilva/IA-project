38c75ff979429f74829c6385d6fc785de9bef55e
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-03-11 08:54:46 +0000

    added group number

--- numbrix.py
+++ numbrix.py
@@ -2,9 +2,9 @@
 # Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
 # Além das funções e classes já definidas, podem acrescentar outras que considerem pertinentes.
 
-# Grupo 00:
-# 00000 Nome1
-# 00000 Nome2
+# Grupo 10:
+# 95533 André Martins Esgalhado
+# 95574 Filipe Ligeiro Silva
 
 import sys
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
