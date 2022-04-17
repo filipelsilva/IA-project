a777a106d628c11c64918ec9a5c272832c6cfb1a
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-04-17 14:40:55 +0100

    remade actions

--- numbrix.py
+++ numbrix.py
@@ -12,10 +12,7 @@
 from __future__ import annotations
 
 import sys
-from tkinter.messagebox import NO
 from search import Problem, Node, astar_search, breadth_first_tree_search, depth_first_tree_search, greedy_search, recursive_best_first_search
-from utils import unique
-from itertools import chain, combinations #TODO podemos importar isto? (é usado no utils)
 
 class NumbrixState:
     state_id = 0
@@ -177,105 +174,35 @@ class Numbrix(Problem):
         """ O construtor especifica o estado inicial. """
         self.initial = NumbrixState(board)
 
-    def is_valid_action(self, state: NumbrixState, action: tuple[int, int, int]) -> bool:
-        """ Retorna um booleano referente à possibilidade de executar a 'action'
-        passada como argumento sobre o 'state' passado como argumento.
-        Verifica se a posicao da 'action' pertence ao 'board' e se o valor
-        aplicado é plausível. """
-
-        #TODO é possível por isto a cortar jogadas q nao vao fazer sentido no futuro
-        # mas dá um bcd trabalho e se calhar a heuristica faz isto / é suficiente
-
-        if (action[0] < 0 or action[0] >= state.board.N
-                or action[1] < 0 or action[1] >= state.board.N):
-            return False
-
-        adjacents = state.board.adjacent_vertical_numbers(action[0], action[1]) 
-        adjacents += state.board.adjacent_horizontal_numbers(action[0], action[1])
-
-        if ((action[2] != state.board.N and action[2]+1 not in adjacents and state.board.find_number(action[2]+1) != (None, None))
-                or (action[2] != 1 and action[2]-1 not in adjacents and state.board.find_number(action[2]-1) != (None, None))):
-            return False
-
-        if (action[2] in adjacents
-                or action[2] > state.board.N**2
-                or action[2] < 1
-                or state.board.find_number(action[2]) != (None, None)):
-            return False
-        
-        if(action[2] != 1 
-                and action[2] != state.board.N
-                and adjacents.count(0) == 0 
-                and (action[2]-1 not in adjacents or action[2]+1 not in adjacents)):
-            return False
-        
-        new_state = self.result(state, action)
-        possible_values = new_state.board.possible_values()
-
-        for val in adjacents:
-            if (val is not None):
-                row, col = new_state.board.find_number(val)
-                val_adjacents = new_state.board.adjacent_vertical_numbers(row, col) 
-                val_adjacents += new_state.board.adjacent_horizontal_numbers(row, col)
-                if (val_adjacents.count(0) == 0 and ((val+1 in possible_values and val+1 not in val_adjacents) 
-                        or (val-1 in possible_values and val-1 not in val_adjacents))):
-                    return False
-
-        for row in range(new_state.board.N):
-            for col in range(new_state.board.N):
-                val = new_state.board.get_number(row, col)
-                if val == 0:
-                    adjacents = new_state.board.adjacent_vertical_numbers(row, col) 
-                    adjacents += new_state.board.adjacent_horizontal_numbers(row, col)
-                    if (adjacents.count(0) == 0):
-                        pairs = list(chain.from_iterable(combinations(adjacents, r) for r in range(2,3)))[1:]
-                        for pair in pairs:
-                            if (pair[1] is None or pair[0] is None):
-                                continue
-                            if (pair[1] - pair[0] == 2 and pair[1]-1 not in possible_values):
-                                return False
-
-        return True
-
     def actions(self, state: NumbrixState) -> list[tuple[int, int, int]]:
         """ Retorna uma lista de ações que podem ser executadas a
         partir do estado passado como argumento. """
-        # FIXME se não for usado apagar funções
-        # max_row, max_col, maximum = state.board.find_maximum()
-        # min_row, min_col, minimum = state.board.find_mininum()
-        possible_vals = state.board.possible_values()
         ret = []
-        for row in range(state.board.N):
-            for col in range(state.board.N):
-                value = state.board.get_number(row, col)
-                if (value == 0):
-                    adjacents = state.board.adjacent_vertical_numbers(row, col)
-                    adjacents += state.board.adjacent_horizontal_numbers(row, col)
-                    
-                    if (adjacents.count(0) == 0):
-                        pairs = list(chain.from_iterable(combinations(adjacents, r) for r in range(2,3)))[1:]
-                        add_to_ret = []
-                        for pair in pairs:
-                            if (pair[1] is None or pair[0] is None):
-                                continue
-                            if (pair[1] - pair[0] == 2 and self.is_valid_action(state, (row, col, pair[1]-1))):
-                                return [(row, col, pair[1]-1)]
-                            
-                        else:
-                            test = [i + 1 for i in adjacents if i != None and i != 0 and i != state.board.N]
-                            test += [i - 1 for i in adjacents if i != None and i != 0 and i != 1]                            
-                            for val in test:
-                                if (val in possible_vals and self.is_valid_action(state, (row, col, val))):
-                                    add_to_ret += [(row, col, val)]
-                            if adjacents.count(0) == 0:
-                                return unique(add_to_ret)
+        possible_values = state.board.possible_values()
 
-                    test = [i + 1 for i in adjacents if i != None and i != 0 and i != state.board.N]
-                    test += [i - 1 for i in adjacents if i != None and i != 0 and i != 1]
-                    for val in test:
-                        if (val in possible_vals and self.is_valid_action(state, (row, col, val))):
-                            ret += [(row, col, val)]
-        return unique(ret)
+        for possible in possible_values:
+            # Ver se i +- 1 tem posições adjacentes livres
+            to_check = []
+            if possible - 1 not in possible_values:
+                to_check += [state.board.find_number(possible - 1)]
+            if possible + 1 not in possible_values:
+                to_check += [state.board.find_number(possible + 1)]
+
+            for row, col in to_check:
+                # Ver se existe alguma posição livre nos adjacentes
+                horizontal = state.board.adjacent_horizontal_numbers(row, col)
+                vertical = state.board.adjacent_vertical_numbers(row, col)
+
+                if horizontal[0] == 0:
+                    ret += [(row, col - 1, possible)]
+                elif horizontal[1] == 0:
+                    ret += [(row, col + 1, possible)]
+                elif vertical[0] == 0:
+                    ret += [(row + 1, col, possible)]
+                elif vertical[1] == 0:
+                    ret += [(row - 1, col, possible)]
+
+        return ret
 
     def result(self, state: NumbrixState, action) -> NumbrixState:
         """ Retorna o estado resultante de executar a 'action' sobre
