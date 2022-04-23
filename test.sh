7f2d90c22648e81c51f4f88239d455f04266eb84
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2022-04-23 19:08:10 +0100

    added test script and output for test 0

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!env sh
+trap ctrl_c INT
+
+function ctrl_c() {
+	echo "Aborted."
+	rm tmp
+	exit
+}
+
+for i in {0..10}; do
+	echo "tests/input${i}.txt"
+	python numbrix.py tests/input${i}.txt > tmp
+	cat tmp
+	diff tmp tests/output${i}.txt
+	echo ""
+done
+
+rm tmp
